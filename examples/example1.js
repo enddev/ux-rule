1d8497ac5f2758d56a387e5ccb2914acf1c42d8c
enddev <eduggan@engr2-5-159-dhcp.int.colorado.edu>
2015-01-29 17:40:48 -0700

    Added complete allImageHasAltText rule
    Also sets a framework for the no404Pages rule

--- examples/example1.js
+++ examples/example1.js
@@ -1,5 +1,5 @@
-var ux = require('../lib'),
-    fs = require('fs')
+var ux = require('../lib');
+var fs = require('fs');
 
 var cnn = fs.readFileSync('./data/www.cnn.com.html','utf8')
 var ret = ux.check(cnn, '<img alt=".*">')
@@ -8,4 +8,13 @@ console.log(ret)
 
 var cu = fs.readFileSync('./data/www.colorado.edu.html','utf8')
 var ret = ux.check(cu, '<img alt=".*">')
+console.log(ret)
 // ==> true or false
+
+var cu = fs.readFileSync('./data/cs.nmt.edu.html','utf8')
+var ret = ux.check(cu, '<img alt=".*">')
+console.log(ret)
+// ==> true or false
+
+var ret = ux.check('<h1><img src="images/ajax-loader.gif" alt="ajax-loader" /><img src="images/ajax-loader.gif" alt="ajax-loader" /><img src="images/ajax-loader.gif" alt="ajax-loader" /><img src="images/ajax-loader.gif" alt="ajax-loader" /></h1>','<img alt=".*">')
+console.log(ret) 
