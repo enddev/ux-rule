6d7dec31fa85c8352378fd8c6ecd71b30101fe5f
enddev <eduggan@s963208.local>
2015-03-12 19:16:24 -0600

    Added more content testing functions and jsonfile package
    Added functions to handle more stringent requirements on image alt
    text, functions to deal with section languages, function to check for
    content skip links, and added certification report exporting to JSON
    file.

--- examples/example1.js
+++ examples/example1.js
@@ -2,18 +2,21 @@ var ux = require('../lib');
 var fs = require('fs');
 
 //var cnn = fs.readFileSync('./data/www.cnn.com.html','utf8')
-var cnn = 'http://www.cnn.com'
-ux.check(cnn, '<img alt=".*">')
+//var cnn = 'http://www.cnn.com'
+//ux.check(cnn, '<img alt=".*">')
 // ==> true or false
 
 //var cu = fs.readFileSync('./data/www.colorado.edu.html','utf8')
-var cu = 'http://google.fr'
+//var cu = 'http://google.fr'
+var cu = 'http://cs.nmt.edu'
+//var cu = 'http://www.squidlord.com/home/writings/fantasy?gclid=COjCrNz_lsQCFQaSaQodf5QAgA'
+//var cu = 'http://www.colorado.edu'
 ux.check(cu, '<img alt=".*">')
 // ==> true or false
 
 //var nf = fs.readFileSync('./data/Object not found!.html','utf8')
-var nf = 'http://www-ia.hiof.no/~linettev/html4U/form.htm'
-ux.check(nf, '<img alt=".*">')
+//var nf = 'http://www-ia.hiof.no/~linettev/html4U/form.htm'
+//ux.check(nf, '<img alt=".*">')
 // ==> true or false
 /*
 ux.check('<h1><img src="images/ajax-loader.gif" alt="ajax-loader" /><img src="images/ajax-loader.gif" alt="ajax-loader" /><img src="images/ajax-loader.gif" alt="ajax-loader" /><img src="images/ajax-loader.gif" alt="ajax-loader" /></h1>','<img alt=".*">')
